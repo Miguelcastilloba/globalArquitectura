c64103d209c3a82b5cc0d3463c5516b3a12ede0e
Miguel Castillo Barajas <miguel.castillo@corporativo.rll>
2024-08-25 12:33:39 -0600

    Header

---
+++ src/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="site-header">
+            <div className="site-name">
+                Arquitectura de Software
+            </div>
+        </header>
+    );
+};
+
+export default Header;
